9bb715f87db5943830245c0e6c531448359a5fe5
Olatunji <olayemi289@gmail.com>
2020-07-31 11:59:07 +0100

    Just created company  migrations for the project

---
+++ database/migrations/2020_07_31_104824_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('logo');
+            $table->text('email');
+            $table->text('address');
+            $table->text('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
